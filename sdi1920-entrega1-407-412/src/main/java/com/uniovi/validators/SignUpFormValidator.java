cf34504723d53905544906194f5ec888b305638e
UO264446 <uo264446@uniovi.es>
2020-03-09 01:45:06 +0100

    Actualización casos SignUpValidator

--- sdi1920-entrega1-407-412/src/main/java/com/uniovi/validators/SignUpFormValidator.java
+++ sdi1920-entrega1-407-412/src/main/java/com/uniovi/validators/SignUpFormValidator.java
@@ -23,17 +23,17 @@ public class SignUpFormValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
+		//El email es obligatorio
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Error.empty");
+		//El nombre y los apellidos son obligatorios
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "Error.empty");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "Error.empty");
 
-
+		//El email no está repetido
 		if (usersService.getUserByEmail(user.getEmail()) != null) {
 			errors.rejectValue("email", "Error.signup.email.duplicate");
 		}
-
-		if (user.getPassword().length() < 5 || user.getPassword().length() > 24) {
-			errors.rejectValue("password", "Error.signup.password.length");
-		}
-
+		//Las contraseñas coinciden
 		if (!user.getPasswordConfirm().equals(user.getPassword())) {
 			errors.rejectValue("passwordConfirm", "Error.signup.passwordConfirm.coincidence");
 		}
